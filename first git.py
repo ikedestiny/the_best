aefa2358d223bbbbe892af339f7449f7fed4c545
ikedestiny <121898018+ikedestiny@users.noreply.github.com>
2023-02-13 00:47:19 +0300

    Create first git.py

---
+++ first git.py
@@ -0,0 +1,13 @@
+""" how to create
+a class in python"""
+
+class point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+#testing the class
+
+p = point(7,8)
+print(p.x)
+print(p.y)
